bbd7f444fc59ec9ea4432030d0d3e7e4aa46389d
a765032380 <a765032380@qq.com>
2021-07-13 18:35:29 +0800

    1

--- app/src/main/java/pub/gll/testmaven/MainActivity.kt
+++ app/src/main/java/pub/gll/testmaven/MainActivity.kt
@@ -2,10 +2,13 @@ package pub.gll.testmaven
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import pub.gll.gtoollib.ext.gone
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.mTV).gone()
     }
 }
